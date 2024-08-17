9be7b1d7585f7aeb4aa16d1363caf92dad154354
thajucp123 <thajucp123@gmail.com>
2024-08-17 18:16:25 +0530

    used better strategy for active link of navbar using useLocation() of react-router-dom

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Head from "./Head";
 import "./header.css";
 
@@ -7,6 +7,7 @@ const Header = () => {
     const [active, setActive] = useState('home');
     const [mobile, setMobile] = useState(false);
     const [scrolled, setScrolled] = useState(false);
+    const location = useLocation();
 
     useEffect(() =>{
         const handleScroll = () => {
@@ -20,19 +21,45 @@ const Header = () => {
         };
     },[])
 
+    useEffect(() => {
+        switch(location.pathname) {
+            case '/':
+                setActive('home');
+                break;
+            case '/about':
+                setActive('about');
+                break;
+            case '/services':
+                setActive('services');
+                break;
+            case '/pricing':
+                setActive('pricing');
+                break;
+            case '/doctors':
+                setActive('doctors');
+                break;
+            case '/contact':
+                setActive('contact');
+                break;
+            default:
+                setActive('home');
+                break;
+        }
+    },[location])
+
     return(
         <section className="header-container">
          <Head/>
          <header className={scrolled ? "scrolled" : ""}>
             <nav className="flexSB">
                 <ul className= {mobile? "mobile-nav" : "flexSB"} onClick={() => setMobile(false)}>
-                    <li><Link to="/" className={active === 'home' ?"active":""} onClick={()=>setActive('home')}>Home</Link></li>
-                    <li><Link to="/about" className={active === 'about' ?"active":""} onClick={()=>setActive('about')}>About Us</Link></li>
-                    <li><Link to="/services" className={active === 'services' ?"active":""} onClick={()=>setActive('services')}>Services</Link></li>
-                    <li><Link to="/pricing" className={active === 'pricing' ?"active":""} onClick={()=>setActive('pricing')}>Packages</Link></li>
-                    <li><Link to="/doctors" className={active === 'doctors' ?"active":""} onClick={()=>setActive('doctors')}>Doctors</Link></li>
-                    <li><Link to="/gallery" className={active === 'gallery' ?"active":""} onClick={()=>setActive('gallery')}>Gallery</Link></li>
-                    <li><Link to="/contact" className={active === 'contact' ?"active":""} onClick={()=>setActive('contact')}>Contact Us</Link></li>
+                    <li><Link to="/" className={active === 'home' ?"active":""}>Home</Link></li>
+                    <li><Link to="/about" className={active === 'about' ?"active":""}>About Us</Link></li>
+                    <li><Link to="/services" className={active === 'services' ?"active":""}>Services</Link></li>
+                    <li><Link to="/pricing" className={active === 'pricing' ?"active":""}>Packages</Link></li>
+                    <li><Link to="/doctors" className={active === 'doctors' ?"active":""}>Doctors</Link></li>
+                    <li><Link to="/gallery" className={active === 'gallery' ?"active":""}>Gallery</Link></li>
+                    <li><Link to="/contact" className={active === 'contact' ?"active":""}>Contact Us</Link></li>
                 </ul>
                 <Link to="/appointments">
                 <div className='start'>
