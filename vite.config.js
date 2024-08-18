f4e0fe2d7da1d069fe49e45ee149b3e53dd51a78
thajucp123 <thajucp123@gmail.com>
2024-08-18 19:38:32 +0530

    built the project and fixed issues for regular hosting servers by adding an htaccess file and seting base path to './' in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages';
+import generateSitemap from 'vite-plugin-pages-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',  
+  plugins: [react(),
+    Pages({
+      onRoutesGenerated: (routes) => {
+        generateSitemap({
+          routes,
+          hostname: 'http://thajucp.byethost10.com',  // Replace with actual domain
+          outDir: 'dist',
+          readable: true,
+        });
+      },
+    }),
+  ],
   server: {
     host: '0.0.0.0',
      port: 5173
